18b50fbdf81f176547d9341e69500801ebe8d585
hwthomas <hywel.w.thomas@gmail.com>
2019-03-01 12:28:18 +0000

    Combine folders and add .neutrinorc.js file copied from original

---
+++ src/safenetwork.org.js
@@ -0,0 +1,28 @@
+
+async function authoriseAndConnect() {
+}
+
+async function createMutableData() {
+}
+
+async function getItems() {
+  return [];
+};
+
+async function insertItem(key, value) {
+};
+
+async function updateItem(key, value, version) {
+};
+
+async function deleteItems(items) {
+};
+
+module.exports = {
+  authoriseAndConnect,
+  createMutableData,
+  getItems,
+  insertItem,
+  updateItem,
+  deleteItems
+};
